7b950c994eb8e2de4350c1676988e980dc03ba0c
Siva <siva9kolli@gmail.com>
2025-04-17 16:54:28 +0530

    data provider testng with example

--- src/main/java/com/a3/automation/BaseA3.java
+++ src/main/java/com/a3/automation/BaseA3.java
@@ -8,10 +8,7 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.ITestResult;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Listeners;
+import org.testng.annotations.*;
 
 import java.io.ByteArrayInputStream;
 import java.io.File;
@@ -29,6 +26,11 @@ public class BaseA3 {
         driver.get("https://www.saucedemo.com/");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+    }
+
+    @BeforeMethod
+    public void launchApp(){
+        driver.get("https://www.saucedemo.com/");
     }
 
     @AfterTest
